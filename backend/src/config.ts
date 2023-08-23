f4268721bad0171fed24a54ab665e28e40e0c6bf
unknown <oissac22@hotmail.com>
2023-08-23 18:59:12 -0300

    enverimonts defined

---
+++ backend/src/config.ts
@@ -0,0 +1,4 @@
+export const AUTHORIZATION_WEB = process.env.AUTHORIZATION_WEB || "" ;
+export const AUTHORIZATION_MOBILE = process.env.AUTHORIZATION_MOBILE || "" ;
+export const KEY_LOGIN_LONG_TIME = process.env.KEY_LOGIN_LONG_TIME || "" ;
+export const KEY_LOGIN_SHORT_TIME = process.env.KEY_LOGIN_SHORT_TIME || "" ;
