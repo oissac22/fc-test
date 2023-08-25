10995a525623e04e7387ac7961764098f3d86c24
unknown <oissac22@hotmail.com>
2023-08-25 14:37:51 -0300

    delete controller ok

---
+++ backend/src/users/controller/ControllerUserDeleteById.ts
@@ -0,0 +1,20 @@
+import { IController, IControllerProps, TControllerExec } from "../../interfaces/Controller";
+import { IServicesUsers } from "../../interfaces/IModelUsers";
+
+export class ControllerUserDeleteById implements IController {
+
+    constructor(
+        private readonly service: IServicesUsers
+    ) { }
+
+    async exec(props: IControllerProps): Promise<TControllerExec>
+    {
+        const { id = '0' } = props.params || {};
+        await this.service.deleteUser(Number(id || 0));
+        return {
+            status: 200,
+            data: '',
+        }
+    }
+
+}
--- backend/src/users/index.ts
+++ backend/src/users/index.ts
@@ -1,6 +1,6 @@
 import { Api } from "../api";
 import { Database } from "../databasesql";
-import { ControllerUserInsertData, ControllerUserUpdateData, ControllerUsersList } from "./controller";
+import { ControllerUserDeleteById, ControllerUserInsertData, ControllerUserUpdateData, ControllerUsersList } from "./controller";
 import { ControllerUsersDataById } from './controller/ControllerUsersDataById';
 import { ModelUsers } from "./model";
 import { ServiceUsers } from "./service";
@@ -12,3 +12,4 @@ Api.get('/api/v1/users', new ControllerUsersList(service))
 Api.get('/api/v1/users/:id', new ControllerUsersDataById(service))
 Api.post('/api/v1/users', new ControllerUserInsertData(service))
 Api.put('/api/v1/users/:id', new ControllerUserUpdateData(service))
+Api.delete('/api/v1/users/:id', new ControllerUserDeleteById(service))
