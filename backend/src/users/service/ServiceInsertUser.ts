c386f345ba484472a0cce3f2722dfdd59b54d972
unknown <oissac22@hotmail.com>
2023-08-25 11:37:52 -0300

    adjust ServiceUpdateUser

--- backend/src/users/service/ServiceInsertUser.ts
+++ backend/src/users/service/ServiceInsertUser.ts
@@ -2,6 +2,7 @@ import { adjustPhoneDDIOnlyNumber } from "../../entities/adjustPhoneDDIOnlyNumbe
 import { cpfAdjustToInsert } from "../../entities/cpfInsert";
 import { HTTPException, HTTPStatus } from "../../entities/error";
 import { phoneAdjustToInsert } from "../../entities/phoneInsert";
+import { Verify } from "../../entities/verify";
 import { IModelUsers, IUsersDataInsert } from "../../interfaces/IModelUsers";
 
 
@@ -22,21 +23,21 @@ export class ServiceInsertUser {
     private verifyData()
     {
         const { age, cpf, email, login, mather, name, password, phone, status } = this.data;
-        if (!/[^ ]+@[^ ]+\.[^ ]+/.test(email))
+        if (!Verify.email(email))
             throw new HTTPException(`"${email}" não é um e-mail válido`, HTTPStatus.NOT_ACCEPTABLE, 'notValidEmail');
-        if (!/^\d{10,13}$/.test(phone))  
+        if (!Verify.phone(phone))  
             throw new HTTPException(`"${phone}" não é um telefone válido`, HTTPStatus.NOT_ACCEPTABLE, 'notValidPhone');
-        if(status && ["block", "inactive", "active"].indexOf(status) < 0)
+        if(status && !Verify.statusUser(status))
             throw new HTTPException(`"${status}" não é um status válido`, HTTPStatus.NOT_ACCEPTABLE, 'notValidStatus');
-        if (!/\w{2,}/.test(name))
+        if (!Verify.nameUser(name))
             throw new HTTPException(`"${name}" não é um nome válido`, HTTPStatus.NOT_ACCEPTABLE, 'notValidName');
-        if (!/^\d{11}$/.test(cpf))
+        if (!Verify.cpf(cpf))
             throw new HTTPException(`"${cpf}" não é um cpf válido`, HTTPStatus.NOT_ACCEPTABLE, 'notValidCPF');
-        if (!/\w{2,}/.test(login))
+        if (!Verify.login(login))
             throw new HTTPException(`"${login}" não é um login válido`, HTTPStatus.NOT_ACCEPTABLE, 'notValidLogin');
-        if (!/\w{6,}/.test(password))
+        if (!Verify.password(password))
             throw new HTTPException(`"${password}" não é uma senha válida, a senha deve conter 6 ou mais digitos`, HTTPStatus.NOT_ACCEPTABLE, 'notValidPassword');
-        if (!age?.getTimezoneOffset || !(age.getTimezoneOffset()))
+        if (!Verify.date(age))
             throw new HTTPException(`"${age}" não é uma data de nascimento válida`, HTTPStatus.NOT_ACCEPTABLE, 'notValidAge');
     }
 
