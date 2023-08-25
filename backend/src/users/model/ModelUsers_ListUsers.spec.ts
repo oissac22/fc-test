ab6e63a169ae30f0aaa3b06bcbc72103d29c9c27
unknown <oissac22@hotmail.com>
2023-08-25 10:21:20 -0300

    adjust ModelUsers_ListUsers class

--- backend/src/users/model/ModelUsers_ListUsers.spec.ts
+++ backend/src/users/model/ModelUsers_ListUsers.spec.ts
@@ -30,7 +30,7 @@ describe("ModelUsers_ListUsers class", () => {
         const { model, database } = newModelUsers_ListUsers({ index:0, limit:3 });
         const sut = await model.result();
         expect(sut).toEqual(FAKE_LIST);
-        expect(database._query).toBe(`SELECT id, name, email, phone\nFROM users\nLIMIT ?\nOFFSET ?`);
+        expect(database._query).toBe(`SELECT id, name, email, phone\nFROM users\nORDER BY name\nLIMIT ?\nOFFSET ?`);
         expect(database._props).toEqual([3, 0]);
     })
 
@@ -39,8 +39,9 @@ describe("ModelUsers_ListUsers class", () => {
         await model.result();
         expect(database._query).toBe(`SELECT id, name, email, phone\nFROM users\n` +
             `WHERE id=? OR name LIKE ? OR email LIKE ? OR phone LIKE ? OR cpf LIKE ? OR mather LIKE ? OR status LIKE ?\n` +
+            `ORDER BY name\n`+
             `LIMIT ?\nOFFSET ?`);
-        const expectResult = [...(new Array(7).fill("filter test")), 1, 0];
+        const expectResult = [...(new Array(7).fill("%filter%test%")), 1, 0];
         expect(database._props).toEqual(expectResult);
     })
 
