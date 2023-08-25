0a03b21d70c7f38662ac2b144b6bc9aedfef02be
unknown <oissac22@hotmail.com>
2023-08-25 10:16:58 -0300

    adjust filter model users

--- backend/src/users/model/ModelUsers_ListUsers.ts
+++ backend/src/users/model/ModelUsers_ListUsers.ts
@@ -3,12 +3,14 @@ import { ISQL } from "../../interfaces/ISQL";
 
 const SQL = `SELECT id, name, email, phone
 FROM users
+ORDER BY name
 LIMIT ?
 OFFSET ?`;
 
 const SQL_WITH_SQL = `SELECT id, name, email, phone
 FROM users
 WHERE id=? OR name LIKE ? OR email LIKE ? OR phone LIKE ? OR cpf LIKE ? OR mather LIKE ? OR status LIKE ?
+ORDER BY name
 LIMIT ?
 OFFSET ?`;
 
@@ -23,7 +25,7 @@ export class ModelUsers_ListUsers {
         if (this.props.filter)
         {
             const { filter } = this.props;
-            const filters = (new Array(7)).fill(filter);
+            const filters = (new Array(7)).fill('%' + filter.replace(/[^a-zA-Z0-9]+/g,'%') + '%');
             return await this.database.list(SQL_WITH_SQL, [...filters, this.props.limit, this.props.index]);
         }
         else
