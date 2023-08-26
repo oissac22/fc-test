227d763c4c6899f22108924657e06682cde7f400
unknown <oissac22@hotmail.com>
2023-08-26 14:33:03 -0300

    define IServiceLogin interface

--- backend/src/interfaces/IServiceLogin.ts
+++ backend/src/interfaces/IServiceLogin.ts
@@ -1,6 +1,17 @@
-export interface IServiceLogin {
+export interface IServiceLoginLogin {
     login(login:string, password:string):Promise<{ token:string }>;
+}
+
+export interface IServiceLoginLogoff {
     logoff(login:string, password:string):Promise<void>;
+}
+
+export interface IServiceLoginLogoffEveryMachines {
     logoffEveryMachines(login:string, password:string):Promise<void>;
+}
+
+export interface IServiceLoginVerifyLoginActived {
     verifyLoginActived(token:string):Promise<boolean>;
 }
+
+export interface IServiceLogin extends IServiceLoginLogin, IServiceLoginLogoff, IServiceLoginLogoffEveryMachines, IServiceLoginVerifyLoginActived {}
