40d4a2b1f2fcd456d41ff692a002deaf6bbefa76
unknown <oissac22@hotmail.com>
2023-08-27 20:54:05 -0300

    define about ok

---
+++ frontend2/src/pages/About.tsx
@@ -0,0 +1,11 @@
+import { CenterItem } from "../components/centerItem";
+
+export function About()
+{
+    return <CenterItem>
+        <p>Sobre</p>
+        <div>
+            Um exemplo de segunda página representando o SOBRE.
+        </div>
+    </CenterItem>
+}
--- frontend2/src/pages/index.tsx
+++ frontend2/src/pages/index.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import { Home } from "./Home";
 import { Logoff } from "./Logoff";
+import { About } from "./About";
 
 export function PagesRouter()
 {
     return <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/logoff" element={<Logoff />} />
+        <Route path="/about" element={<About />} />
     </Routes>
 }
